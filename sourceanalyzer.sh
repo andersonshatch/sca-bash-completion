919970d5dc42f1ac66f962ce4ef1d61ffd9632d2
Josh Anderson <andersonshatch@gmail.com>
2013-11-03 20:39:03 +0000

    Add clobber log switch.

--- sourceanalyzer.sh
+++ sourceanalyzer.sh
@@ -11,7 +11,7 @@ _sourceanalyzer()
 	cur="${COMP_WORDS[COMP_CWORD]}"					#Current word
 	prev="${COMP_WORDS[COMP_CWORD-1]}"				#Previous word
 
-	opts="-64 -append -b -build-label -build-project -build-version -cp -classpath -clean"
+	opts="-64 -append -b -build-label -build-project -build-version -cp -classpath -clean -clobber-log"
 	opts="$opts -debug -debug-verbose -disable-default-rule-type -disable-source-rendering -dotnet-sources"
 	opts="$opts -encoding -exclude -f -filter -findbugs -format -h -help -j -libdirs -logfile"
 	opts="$opts -no-default-issue-rules -no-default-rules -no-default-source-rules -no-default-sink-rules"
